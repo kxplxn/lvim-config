6653a47979dffcad7df7bcbba18eac17d03fa1be
Atlas Kaplan <aakaplan@proton.me>
2023-11-26 21:59:34 +0000

    neotest c/p config and kanagawa

--- config.lua
+++ config.lua
@@ -21,6 +21,13 @@ lvim.builtin.nvimtree.setup.view.width = 100
 
 -- plugins
 lvim.plugins = {
+  -- themes
+  { 'projekt0n/github-nvim-theme' },
+  { "bluz71/vim-moonfly-colors" },
+  { 'dasupradyumna/midnight.nvim' },
+  { "rebelot/kanagawa.nvim" },
+
+  -- other
   {
     "sainnhe/gruvbox-material",
     init = function()
@@ -35,35 +42,64 @@ lvim.plugins = {
       "nvim-neotest/neotest-go",
       -- Your other test adapters here
     },
-    config = function()
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          args = { "-tags=unit,integration,utest,itest" },
+        },
+      },
+    },
+    config = function(_, opts)
       -- get neotest namespace (api call creates or returns namespace)
       local neotest_ns = vim.api.nvim_create_namespace("neotest")
       vim.diagnostic.config({
         virtual_text = {
           format = function(diagnostic)
-            local message =
-                diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            -- Replace newline and tab characters with space for more compact diagnostics
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
             return message
           end,
         },
       }, neotest_ns)
-      require("neotest").setup({
-        -- your neotest config here
-        adapters = {
-          require("neotest-go"),
-        },
-      })
+
+      if opts.adapters then
+        local adapters = {}
+        for name, config in pairs(opts.adapters or {}) do
+          if type(name) == "number" then
+            if type(config) == "string" then
+              config = require(config)
+            end
+            adapters[#adapters + 1] = config
+          elseif config ~= false then
+            local adapter = require(name)
+            if type(config) == "table" and not vim.tbl_isempty(config) then
+              local meta = getmetatable(adapter)
+              if adapter.setup then
+                adapter.setup(config)
+              elseif meta and meta.__call then
+                adapter(config)
+              else
+                error("Adapter " .. name .. " does not support setup")
+              end
+            end
+            adapters[#adapters + 1] = adapter
+          end
+        end
+        opts.adapters = adapters
+      end
+
+      require("neotest").setup(opts)
     end,
   },
   { "leoluz/nvim-dap-go" }
 }
 
-lvim.colorscheme = "gruvbox-material"
+lvim.colorscheme = "kanagawa"
 
 -- keymap
 lvim.keys.normal_mode["<A-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<A-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["<C-i>"] = "<cmd>lua vim.diagnostic.open_float()<cr>"
+lvim.keys.normal_mode["<C-e>"] = "<cmd>lua vim.diagnostic.open_float()<cr>"
 
 local wk = require("which-key")
 wk.register({
